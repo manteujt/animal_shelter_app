9a7868781312fd6d0a4806c6388d1973fc13dec1
manteujt <manteujt@uwec.edu>
2012-07-09 10:16:20 -0500

    Improve the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalShelterApp::Application.config.secret_token = 'a4a5c15b13b8c9c278ec616b63a26da19b0d21a0ee7eef4edfc73354ab7cc524f26a1ce4b13a8adb1ce968bf2b050f1d0a7c6838ebe84a2f94c58c168bd9c0d9'
